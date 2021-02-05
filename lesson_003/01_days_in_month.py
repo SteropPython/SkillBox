a21eab22afed2d485aa9c14c60d78d358909edb4
Sterop <steroppython@gmail.com>
2021-02-05 11:40:30 +0300

    lesson 3 task 1

--- lesson_003/01_days_in_month.py
+++ lesson_003/01_days_in_month.py
@@ -12,3 +12,13 @@ month = int(user_input)
 print('Вы ввели', month)
 
 # TODO здесь ваш код
+
+if 0 < month < 13:
+    if month in [1, 3, 5, 7, 8, 10, 12]:
+        print('31 day in yours month number')
+    elif month in [4, 6, 9, 11]:
+        print('30 day in yours month number')
+    else:
+        print('28 day in yours month number')
+else:
+    print('Month may be from 1 to 12, jerk')
