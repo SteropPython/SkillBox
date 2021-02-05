4492c6de01f73fa1e2ce1e4e073f6febbcebfac4
Sterop <steroppython@gmail.com>
2021-02-05 22:32:36 +0300

    lesson 3 task 7

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -3,9 +3,34 @@
 # (цикл for)
 import simple_draw
 
+simple_draw.resolution = (1000, 1000)
+
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
 # TODO здесь ваш код
+
+startPointX = 0
+startPointY = 0
+finishPointX = simple_draw.resolution[0]
+finishPointY = simple_draw.resolution[1]
+brickWidth = 100
+brickHight = 50
+brickStep = 0
+
+for startPointY in range(0, finishPointY, brickHight):
+    for startPointX in range(0, finishPointX, brickWidth):
+        if brickStep % 2 == 0:
+            startPointX -= brickHight
+        startPoint = simple_draw.get_point(startPointX, startPointY)
+        endPoint = simple_draw.get_point(startPointX + brickWidth, startPointY + brickHight)
+        colorRectangle = simple_draw.random_color()
+        simple_draw.rectangle(left_bottom = startPoint, right_top = endPoint, color = colorRectangle, width = 2)
+        startPointX += brickWidth
+    startPointY += brickHight
+    startPointX = 0
+    brickStep += 1
+    
+    
 
 simple_draw.pause()
