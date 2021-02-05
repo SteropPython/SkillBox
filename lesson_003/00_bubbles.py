2fd946d8b2625b9ab3d592dfc10f3a643b376a5d
Sterop <steroppython@gmail.com>
2021-02-05 10:53:39 +0300

    lesson 3 task 0-3

--- lesson_003/00_bubbles.py
+++ lesson_003/00_bubbles.py
@@ -16,17 +16,26 @@ sd.resolution = (1000, 1000)
 # Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
 # TODO здесь ваш код
 
-def bubble(x,y,step):
-    point = sd.get_point(x,y)
-    rad = 60
-    for _ in range(3):
-        sd.circle(center_position=point, radius=rad)
-        rad += step
+# def bubble(x,y,step):
+#     point = sd.get_point(x,y)
+#     rad = 60
+#     for _ in range(3):
+#         sd.circle(center_position=point, radius=rad)
+#         rad += step
 
-bubble(500,500,45)
+# bubble(500,500,45)
 
 # Нарисовать 10 пузырьков в ряд
 # TODO здесь ваш код
+
+rad = 30
+
+x, y, z = 35, 35, -35
+
+for i in range(10):
+    z += 2 * x
+    point = sd.get_point(z,y)
+    sd.circle(center_position=point, radius=rad)
 
 # Нарисовать три ряда по 10 пузырьков
 # TODO здесь ваш код
