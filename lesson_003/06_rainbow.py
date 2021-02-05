dffa3d5d344e6a821ba900a25836d2cbe462a39e
Sterop <steroppython@gmail.com>
2021-02-05 21:19:59 +0300

    lesson 3 task 6-2

--- lesson_003/06_rainbow.py
+++ lesson_003/06_rainbow.py
@@ -12,21 +12,28 @@ rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
 # TODO здесь ваш код
 
-startPointX = 50
-startPointY = 50
-finishPointX = 350
-finishPointY = 450
-for i in range(7):
-    currentColor = rainbow_colors[i]
-    startPoint = sd.get_point(startPointX, startPointY)
-    endPoint = sd.get_point(finishPointX, finishPointY)
-    sd.line(start_point = startPoint, end_point = endPoint, color=currentColor, width = 15)
-    startPointX += 15
-    finishPointX += 15
+# startPointX = 50
+# startPointY = 50
+# finishPointX = 350
+# finishPointY = 450
+# for i in range(7):
+#     currentColor = rainbow_colors[i]
+#     startPoint = sd.get_point(startPointX, startPointY)
+#     endPoint = sd.get_point(finishPointX, finishPointY)
+#     sd.line(start_point = startPoint, end_point = endPoint, color=currentColor, width = 15)
+#     startPointX += 15
+#     finishPointX += 15
 
 # Усложненное задание, делать по желанию.
 # Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
 # поэкспериментировать с параметрами, что бы было красиво
 # TODO здесь ваш код
+
+pointStartRainbow = sd.get_point(0,0)
+radiusRainbow = 500
+for i in range(7):
+    currentColor = rainbow_colors[i]
+    sd.circle(center_position=pointStartRainbow, radius=radiusRainbow, color = currentColor, width=50)
+    radiusRainbow += 50
 
 sd.pause()
