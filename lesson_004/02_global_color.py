a0eb4cef67e18fcc02c63bbbe81bd40d001f275d
Sterop <steroppython@gmail.com>
2021-02-06 22:58:35 +0300

    lesson 4 task 2

--- lesson_004/02_global_color.py
+++ lesson_004/02_global_color.py
@@ -16,4 +16,55 @@ import simple_draw as sd
 
 # TODO здесь ваш код
 
+figure_color = (
+    sd.COLOR_RED,
+    sd.COLOR_ORANGE,
+    sd.COLOR_YELLOW,
+    sd.COLOR_GREEN,
+    sd.COLOR_CYAN,
+    sd.COLOR_BLUE,
+    sd.COLOR_PURPLE
+)
+
+
+def draw_line(sides, start, stop, angle, length, width, colored):
+
+    decode_color = figure_color[colored]
+
+    zero_point = sd.get_point(start, stop)
+
+    amount_sides_angle = int(360 / sides)
+
+    for i in range(0, 360, amount_sides_angle):
+        draw_lines = sd.vector(zero_point, angle, length, decode_color, width)
+        zero_point = sd.get_point(draw_lines.x, draw_lines.y)
+        angle += amount_sides_angle
+
+
+def drawing_figure(start_point_x, start_point_y, start_angle, start_length):
+
+    user_color = {
+        1: 'Red',
+        2: 'Orange',
+        3: 'Yellow',
+        4: 'Green',
+        5: 'Cyan',
+        6: 'Blue',
+        7: 'Purple'
+    }
+
+    draw_sides = int(input('How many sides the figure should have? (more than two)\n', ))
+
+    draw_width = int(input('How width of sides the figure should have?\n', ))
+
+    for i in user_color:
+        print(i, '=>', user_color[i])
+
+    draw_color = int(input('Select number of color for figure:\n', )) - 1
+
+    draw_line(draw_sides, start_point_x, start_point_y, start_angle, start_length, draw_width, draw_color)
+
+
+drawing_figure(150, 150, 10, 50)
+
 sd.pause()
