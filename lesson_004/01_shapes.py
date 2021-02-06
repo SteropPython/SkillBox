30477ebce256ca8e2bcce64c0a74b89fbc7ccde4
Sterop <steroppython@gmail.com>
2021-02-06 19:13:09 +0300

    lesson 4 task 1-1

--- lesson_004/01_shapes.py
+++ lesson_004/01_shapes.py
@@ -29,6 +29,70 @@ import simple_draw as sd
 
 # TODO здесь ваш код
 
+
+def drawing_triangle(start_point_x, start_point_y, start_angle, start_length):
+    zero_point = sd.get_point(start_point_x, start_point_y)
+    draw_line_a = sd.get_vector(zero_point, start_angle, start_length, 1)
+    draw_line_a.draw()
+    draw_line_b = sd.get_vector(draw_line_a.end_point, start_angle + 120, start_length, 1)
+    draw_line_b.draw()
+    draw_line_c = sd.get_vector(draw_line_b.end_point, start_angle + 240, start_length, 1)
+    draw_line_c.draw()
+
+
+drawing_triangle(150, 150, 10, 75)
+
+
+def drawing_square(start_point_x, start_point_y, start_angle, start_length):
+    zero_point = sd.get_point(start_point_x, start_point_y)
+    draw_line_a = sd.get_vector(zero_point, start_angle, start_length, 1)
+    draw_line_a.draw()
+    draw_line_b = sd.get_vector(draw_line_a.end_point, start_angle + 90, start_length, 1)
+    draw_line_b.draw()
+    draw_line_c = sd.get_vector(draw_line_b.end_point, start_angle + 180, start_length, 1)
+    draw_line_c.draw()
+    draw_line_d = sd.get_vector(draw_line_c.end_point, start_angle + 270, start_length, 1)
+    draw_line_d.draw()
+
+
+drawing_square(400, 150, 20, 75)
+
+
+def drawing_pentagon(start_point_x, start_point_y, start_angle, start_length):
+    zero_point = sd.get_point(start_point_x, start_point_y)
+    draw_line_a = sd.get_vector(zero_point, start_angle, start_length, 1)
+    draw_line_a.draw()
+    draw_line_b = sd.get_vector(draw_line_a.end_point, start_angle + 72, start_length, 1)
+    draw_line_b.draw()
+    draw_line_c = sd.get_vector(draw_line_b.end_point, start_angle + 144, start_length, 1)
+    draw_line_c.draw()
+    draw_line_d = sd.get_vector(draw_line_c.end_point, start_angle + 216, start_length, 1)
+    draw_line_d.draw()
+    draw_line_e = sd.get_vector(draw_line_d.end_point, start_angle + 288, start_length, 1)
+    draw_line_e.draw()
+
+
+drawing_pentagon(400, 400, 30, 75)
+
+
+def drawing_hexagon(start_point_x, start_point_y, start_angle, start_length):
+    zero_point = sd.get_point(start_point_x, start_point_y)
+    draw_line_a = sd.get_vector(zero_point, start_angle, start_length, 1)
+    draw_line_a.draw()
+    draw_line_b = sd.get_vector(draw_line_a.end_point, start_angle + 60, start_length, 1)
+    draw_line_b.draw()
+    draw_line_c = sd.get_vector(draw_line_b.end_point, start_angle + 120, start_length, 1)
+    draw_line_c.draw()
+    draw_line_d = sd.get_vector(draw_line_c.end_point, start_angle + 180, start_length, 1)
+    draw_line_d.draw()
+    draw_line_e = sd.get_vector(draw_line_d.end_point, start_angle + 240, start_length, 1)
+    draw_line_e.draw()
+    draw_line_f = sd.get_vector(draw_line_e.end_point, start_angle + 300, start_length, 1)
+    draw_line_f.draw()
+
+
+drawing_hexagon(150, 400, 40, 75)
+
 # Часть 1-бис.
 # Попробуйте прикинуть обьем работы, если нужно будет внести изменения в этот код.
 # Скажем, связывать точки не линиями, а дугами. Или двойными линиями. Или рисовать круги в угловых точках. Или...
