1ebeaecca4f7e06b32f82bdfdc71af6a11bc24bc
Sterop <steroppython@gmail.com>
2021-02-10 12:36:50 +0300

    lesson 5 task 2

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -7,6 +7,29 @@
 
 # TODO здесь ваш код
 
+from district.central_street.house1.room1 import folks as central_folks_room_1_1
+from district.central_street.house1.room2 import folks as central_folks_room_1_2
+from district.central_street.house2.room1 import folks as central_folks_room_2_1
+from district.central_street.house2.room2 import folks as central_folks_room_2_2
 
+from district.soviet_street.house1.room1 import folks as soviet_folks_room_1_1
+from district.soviet_street.house1.room2 import folks as soviet_folks_room_1_2
+from district.soviet_street.house2.room1 import folks as soviet_folks_room_2_1
+from district.soviet_street.house2.room2 import folks as soviet_folks_room_2_2
+
+
+central_district_folks = central_folks_room_1_1 +\
+                         central_folks_room_1_2 +\
+                         central_folks_room_2_1 +\
+                         central_folks_room_2_2
+
+soviet_district_folks = soviet_folks_room_1_1 +\
+                        soviet_folks_room_1_2 +\
+                        soviet_folks_room_2_1 +\
+                        soviet_folks_room_2_2
+
+print('На районе живут:', ', '.join(central_district_folks))
+
+print('На районе живут:', ', '.join(soviet_district_folks))
 
 
